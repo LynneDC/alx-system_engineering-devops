9163b1eb380a30137a61f6636d8c202e6b1fd7e9
LynneDc <roselinedc1308@gmail.com>
2023-10-23 18:29:05 +0200

    more formats added in exporting to json

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
-"""Python script that, using this REST API"""
-
-
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/users"
-    response = requests.get(url)
-    employees = response.json()
-
-    todo_all_employees = {}
-    for employee in employees:
-        employee_id = employee.get("id")
-        employee_name = employee.get("name")
-        url = "https://jsonplaceholder.typicode.com/todos?userId={}"
-        .format(employee_id)
-        response = requests.get(url)
-        todos = response.json()
-        task_list = []
-        for task in todos:
-            task_dict = {"task": task.get("title"),
-                         "completed": task.get("completed"),
-                         "username": employee_name}
-            task_list.append(task_dict)
-        todo_all_employees[employee_id] = task_list
-
-    with open("todo_all_employees.json", "w") as outfile:
-        json.dump(todo_all_employees, outfile)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
