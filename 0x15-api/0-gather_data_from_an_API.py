7084af0b1789f2af374efaa9630bab5154715ada
LynneDc <roselinedc1308@gmail.com>
2023-10-23 16:49:34 +0200

    return to dolist infor of a given user

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Python script that, using this REST API,
+for a given employee ID,
+returns information about TODO list progress.
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} EMPLOYEE_ID")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Error: Employee with ID {employee_id} not found.")
+        sys.exit(1)
+
+    employee_name = response.json().get("name")
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response = requests.get(url)
+    todos = response.json()
+    total_tasks = len(todos)
+    completed_tasks = [todo for todo in todos if todo.get("completed")]
+    num_completed_tasks = len(completed_tasks)
+
+    print(f"Employee {employee_name} is done with tasks(
+            {num_completed_tasks}/{total_tasks}): ")
+    for task in completed_tasks:
+        print(f"\t{task.get('title')}")
